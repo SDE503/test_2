7cd0e29bdfefc01c6774df94772cdda2e08d4984
SDE5O3 <sherif_ej06@hotmail.com>
2022-07-23 17:11:14 +0100

    Refactor code using partial and make app production ready

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,12 +1,56 @@
 class ThoughtsController < ApplicationController
+	before_action :set_thought, only: [:show, :edit, :update, :destroy]
 
 	def show
-		@thought = Thought.find(params[:id])
+		
 	end
 	
 	def index
 		@thoughts = Thought.all
 	end
+	
+	def new
+		@thought = Thought.new
+	end
+	
+	def edit 
+	
+	end
+	
+	def create
+		@thought = Thought.new(thought_params)
+		if @thought.save
+			flash[:notice] = 'Thought was created successfully!'
+			redirect_to @thought
+		else
+			render 'new'
+		end			
+	end
+	
+	def update
+		if @thought.update(thought_params)
+			flash[:notice] = "Thought was succesfully updated."
+			redirect_to @thought
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@thought.destroy
+			flash[:notice] = "Thought was successfully deleted!"
+		redirect_to thoughts_path
+	end 
+	
+	private
+	
+		def set_thought
+			@thought = Thought.find(params[:id])
+		end
+		
+		def thought_params
+			params.require(:thought).permit(:title, :description)
+		end
 
 
 end
