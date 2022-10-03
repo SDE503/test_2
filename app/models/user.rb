71f94817361a2b73f44d7c6e34234c6324d83f4c
SDE5O3 <sherif_ej06@hotmail.com>
2022-10-03 19:27:48 +0100

    Add gem BCrypt to app and test authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+	before_save { self.email = email.downcase }
 	has_many :thoughts
 	validates :username, presence: true, 
 						uniqueness:{case_sensitive: false}, 
@@ -9,5 +10,6 @@ class User < ApplicationRecord
 						uniqueness:{case_sensitive: false}, 
 						length: {maximum: 105},
 						format: { with: VALID_EMAIL_REGEX }
+	has_secure_password
 
 end
